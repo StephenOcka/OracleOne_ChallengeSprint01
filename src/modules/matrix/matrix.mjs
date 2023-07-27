fc589e6d3f8f02e3701a446b28ed770013e5931e
Stephen Ocka <98916874+StephenOcka@users.noreply.github.com>
2023-07-26 21:21:32 -0600

    Refactoring the matrix file

--- src/modules/matrix/matrix.mjs
+++ src/modules/matrix/matrix.mjs
@@ -1,19 +1,44 @@
-import size from "./js/size.mjs";
-import render from "./js/render.mjs";
-
 const fontSize = 11;
 const $canvas = document.querySelector("#matrix");
 let ctx;
 let columns;
 
+function size() {
+  $canvas.width = $canvas.offsetWidth;
+  $canvas.height = $canvas.offsetHeight;
+
+  ctx = $canvas.getContext("2d");
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, $canvas.width, $canvas.height);
+
+  columns = Array(Math.floor($canvas.width / fontSize) + 1).fill(0);
+}
+
+function render() {
+  setInterval(() => {
+    ctx.fillStyle = "#0001";
+    ctx.fillRect(0, 0, $canvas.width, $canvas.height);
+    ctx.fillStyle = "#0f0";
+    ctx.font = `${fontSize}pt monospace`;
+
+    columns.forEach((y, index) => {
+      const text = String.fromCharCode(Math.random() * 122);
+      ctx.fillText(text, index * fontSize, y);
+      const random = 100 + Math.random() * 10000;
+      if (y > random) {
+        columns[index] = 0;
+      } else {
+        columns[index] = y + fontSize;
+      }
+    });
+  }, 50);
+}
+
 export default async function () {
-  const si = size({ canvas: $canvas, ctx: ctx, columns: columns, fontSize: fontSize });
-  ctx = si.ctx;
-  columns = si.columns;
-  render({ canvas: $canvas, ctx: ctx, columns: columns, fontSize: fontSize });
+  size();
+  render();
+
   window.addEventListener("resize", () => {
-    const si = size({ canvas: $canvas, ctx: ctx, columns: columns, fontSize: fontSize });
-    ctx = si.ctx;
-    columns = si.columns;
+    size();
   });
 }
