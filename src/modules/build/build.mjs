6f08665e24c80c1ce31699f1d4a858e0135aa793
Stephen Ocka <98916874+StephenOcka@users.noreply.github.com>
2023-07-26 20:50:45 -0600

    Revert change to IIFE in the build file

--- src/modules/build/build.mjs
+++ src/modules/build/build.mjs
@@ -2,9 +2,7 @@ import home from "./js/home.mjs";
 import notFound from "./js/404.mjs";
 import copy from "./js/_copy.mjs";
 
-(async () => {
-  await home.pack();
-  await notFound.pack();
-  copy("src/config/seo/robots.txt");
-  copy("src/config/seo/sitiemap.xml");
-})();
+await home.pack();
+await notFound.pack();
+copy("src/config/seo/robots.txt");
+copy("src/config/seo/sitiemap.xml");
