af1bff98c33661170292a4f0da255f7696040859
Stephen Ocka <98916874+StephenOcka@users.noreply.github.com>
2023-07-27 10:34:12 -0600

    Changes in the redirection of page 404

--- src/page/404/script.mjs
+++ src/page/404/script.mjs
@@ -1,8 +1,10 @@
+import routing_path from "../../controllers/routing_path.mjs";
 import base from "../../modules/base/base.mjs";
 
 base();
 
 setTimeout(function () {
   const url = new URL(window.location.href);
+  routing_path();
   window.location.href = `${url.origin}/index.html`;
 }, 0);
